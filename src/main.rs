042f754d8876c563f141ccf264267eff241bcd00
Casey Vaughn <casey.vaughn9@aol.com>
2024-12-04 14:43:37 -0600

    Episode 2

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+mod camera;
+mod debug;
+mod movement;
+mod spaceship;
+
+use bevy::prelude::*;
+use camera::CameraPlugin;
+use debug::DebugPlugin;
+use movement::MovementPlugin;
+use spaceship::SpaceshipPlugin;
+
+fn main() {
+    App::new()
+        // Bevy built-ins.
+        .insert_resource(ClearColor(Color::srgb(0.1, 0.0, 0.15)))
+        .insert_resource(AmbientLight {
+            color: Color::default(),
+            brightness: 750.0,
+        })
+        .add_plugins(DefaultPlugins)
+        // User configured plugins.
+        .add_plugins(SpaceshipPlugin)
+        .add_plugins(CameraPlugin)
+        .add_plugins(MovementPlugin)
+        .add_plugins(DebugPlugin)
+        .run();
+}
