d8eb112c84fc5a7fea3edb1417b517334d6dcce8
Casey Vaughn <casey.vaughn9@aol.com>
2024-12-06 21:01:04 -0600

    Revert to minimal setup

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,4 @@
 use bevy::prelude::*;
-use bevy_third_person_camera::*;
-
-mod asset_loader;
-mod asteroids;
-mod camera;
-mod collision_detection;
-mod debug;
-mod despawn;
-mod health;
-mod movement;
-mod schedule;
-mod spaceship;
-mod state;
-
-use asset_loader::AssetLoaderPlugin;
-use asteroids::AsteroidPlugin;
-use camera::CameraPlugin;
-use collision_detection::CollisionDetectionPlugin;
-// use debug::DebugPlugin;
-use despawn::DespawnPlugin;
-use movement::MovementPlugin;
-use schedule::SchedulePlugin;
-use spaceship::SpaceshipPlugin;
-use state::StatePlugin;
 
 fn main() {
     App::new()
@@ -32,17 +8,6 @@ fn main() {
             color: Color::default(),
             brightness: 750.0,
         })
-        .add_plugins((DefaultPlugins, ThirdPersonCameraPlugin))
-        // User configured plugins.
-        .add_plugins(AssetLoaderPlugin)
-        .add_plugins(MovementPlugin)
-        .add_plugins(SpaceshipPlugin)
-        .add_plugins(AsteroidPlugin)
-        .add_plugins(CameraPlugin)
-        .add_plugins(CollisionDetectionPlugin)
-        .add_plugins(DespawnPlugin)
-        .add_plugins(SchedulePlugin)
-        .add_plugins(StatePlugin)
-        // .add_plugins(DebugPlugin)
+        .add_plugins(DefaultPlugins)
         .run();
 }
